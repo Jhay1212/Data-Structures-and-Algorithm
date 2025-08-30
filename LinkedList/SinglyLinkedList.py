aa45da5b81bac52e577f72fae1e1c11844ba95dc
jhay12 <rjhay1070@gmail.com>
2025-08-30 17:11:25 +0800

    added insert function on sibgly linked list

--- LinkedList/SinglyLinkedList.py
+++ LinkedList/SinglyLinkedList.py
@@ -35,6 +35,42 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
         self.length += 1
+        
+    def insert(self, val, index):
+        new_node = Node(val)
+        if index > self.length or index < 0: raise IndexError("Index out of range error") # index out of range base casae
+
+        
+        if index == 0 and self.head is not None:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            # print((self.length + index), self.length, index)
+            # if index < 0 and abs(self.length + index) < self.length:
+            #     index =abs(self.length - index)
+            #     print(index)
+            temp_node = self.head
+            for i in range(index - 1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length += 1 
+        
+    def traverse(self):
+        temp = self.head
+        while temp:
+            print(temp)
+            temp = temp.next
+            
+            
+    def search(self, value):
+        temp = self.head
+        while temp:
+            if temp.value == value:
+                return True
+            temp = temp.next
+        return "Value does not exist"
+    
     
     def __str__(self):
         temp = self.head
@@ -52,6 +88,11 @@ class LinkedList:
 linked1 = LinkedList()
 linked1.append(Node(20))
 linked1.append(Node(22))
+linked1.append(Node(21))
+linked1.append(Node(23))
 
+linked1.insert(Node(69), 3)
+linked1.traverse()
+print(linked1.search(21))
 print(linked1)
 
