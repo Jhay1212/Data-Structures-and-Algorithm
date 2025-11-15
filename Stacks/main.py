f1e3b43f8722622e22dba85683b2e0181fe4e0bb
JhayRivera <jhayjane78@gmail.com>
2025-11-15 18:56:13 +0800

    finished stack

--- Stacks/main.py
+++ Stacks/main.py
@@ -1,3 +1,44 @@
 class Stack(list):
-    def push(self):
-        pass
+    def __init__(self):
+        self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+
+    def peek(self):
+        if not self.items:
+            raise "Stack empty"
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+    
+    def __del__(self):
+        self.items = []
+
+        
+
+    def pop(self):
+        if not self.is_empty:
+            raise "Stack is empty"
+        return self.items.pop()
+    
+    @property
+    def is_empty(self):
+        return self.items == 0
+    
+    def __str__(self):
+        if self.is_empty:
+            print(self.is_empty)
+            raise "Stack is empty"
+        i = [str(x) for x in reversed(self.items)]
+        return "->".join(i)
+    
+
+
+mystack = Stack()
+mystack.push(12)
+mystack.push(123)
+mystack.push(124)
+print(mystack.peek())
+
---
+++ Stacks/stack_link_list.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, val):
+        self.head = val
+        self.next = None
+
+class StackLinkedList:
+    def __init__(self):
+        self.top = None
+        self.size = 0         
+
+    def push(self, val):
+        new_node = Node(val)
+        if self.size == 0:
+            self.top = new_node
+        new_node.next =self.top
+        self.top = new_node
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty:
+            return None
+        popped_node = self.top
+        self.top = self.top.next
+
+        self.size -= 1
+        return popped_node.head
+    
+
+    def peek(self):
+        return self.top
+
+    @property
+    def is_empty(self):
+        return self.length == 0
+stack = StackLinkedList()
+stack.push(1)
+stack.push(12)
+print(stack.pop())
